04ffaa2255219ad2c599e2fe111b6df8a91ad1d4
Tumelo <g17m9389@campus.ru.ac.za>
2019-02-21 15:53:02 +0200

    Yet another commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        static string Prefix(string h)
+        {
+            string[] arr = h.Split(' ');
+            int s = 0;
+            if (h != "")
+            {
+                for (int d = 0; d < arr.Length; d++)
+                {
+                    s += arr[d].Length;
+                }
+                h = $"{s},{arr.Length}:" + h;
+            }
+            else h = "0,0:";
+            return h;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix(""));
         }
     }
 }
