fcf340d24251f8636cb3d9744f1cdc5e59ca80e3
Tumelo <g17m9389@campus.ru.ac.za>
2019-02-22 17:01:59 +0200

    Corrected Tests

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using Problems;
+using System.Collections;
+using System;
 
 namespace Tests
 {
@@ -10,56 +12,39 @@ namespace Tests
         {
         }
 
-        [Test]
-        public void Test1()
+        public class MyTests
         {
-            Program Test1 = new Program();
-            string tester = Test1.Prefix("hello");
-            string tester1 = Test1.Prefix("what   ...  did you say??  ");
-            string tester2 = Test1.Prefix("");
-            Assert.AreEqual("0,0:", tester2);
-            Assert.AreEqual("27,5:what   ...  did you say??  ", tester1);
-            Assert.AreEqual("5,1:hello", tester);
-            Assert.Pass();
+            [TestCaseSource(typeof(MyDataClass), "TestCases")]
+            public string Prefix(string h)
+            {
+                int s = h.Length;
+                string[] delimters = new string[] { " " };
+                string[] arr = h.Split(delimters, StringSplitOptions.RemoveEmptyEntries);
+
+                if (h != "")
+                {
+                    h = $"{s},{arr.Length}:" + h;
+                }
+                else h = "0,0:";
+                return h;
+            }
+        }
+
+        public class MyDataClass
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData("Hello").Returns("5,1:Hello");
+                    yield return new TestCaseData("").Returns("0,0:");
+                    yield return new TestCaseData("what   ...  did you say??  ").Returns("27,5:what   ...  did you say??  ");
+                    yield return new TestCaseData("Oh My Word! ").Returns("12,3:Oh My Word! ");
+                    yield return new TestCaseData("WOW").Returns("3,1:WOW");
+                }
+            }
         }
 
     }
 
-    public class Tests2
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test2()
-        {
-            Program Test2 = new Program();
-            string tester1 = Test2.Prefix("WOW");
-            string h = Test2.Prefix("Oh My Word! ");
-            Assert.AreEqual("12,3:Oh My Word! ", h);
-            Assert.AreEqual("3,1:WOW", tester1);
-            Assert.Pass();
-        }
-
-    }
-    public class Tests3
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test3()
-        {
-            Program Test3 = new Program();
-            string tester1 = Test3.Prefix("As it begins");
-            string test = Test3.Prefix("With a default");
-            Assert.AreEqual("12,3:As it begins", tester1);
-            Assert.AreEqual("14,3:With a default", test);
-            Assert.Pass();
-        }
-    }
 }
